6186377156cd0255489c5e9b542ae829b3b47e3d
Romain <romain.assie@ellona.io>
2022-01-14 17:38:25 +0100

    Add sword to inventory

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 #[derive(Debug)]
+struct Item {
+    name: String,
+    damage: i32
+}
+
+#[derive(Debug)]
 struct Perso {
     name: String,
     life: i32,
+    inventory: Vec<Item>
 }
 
 fn main() {
+    let sword = Item {
+        name: "Big sword".to_string(),
+        damage: 5
+    };
 
     let our_hero = Perso {
         name: "Toto".to_string(),
-        life: 100
+        life: 100,
+        inventory: vec![sword]
     };
 
     println!("Hello, {our_hero:?}");
