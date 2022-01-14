1f65c60b1a0d0d9c8d62f74c7a7fe42d4f0f63d9
Romain <romain.assie@ellona.io>
2022-01-14 17:51:28 +0100

    Add badguy, error for sharing sword

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 trait Actor {
-     fn attack(&self, target: &Actor) -> bool;
+     fn attack(&self, target: &dyn Actor) -> bool;
 }
 
 #[derive(Debug)]
@@ -15,6 +15,13 @@ struct Perso {
     inventory: Vec<Item>
 }
 
+#[derive(Debug)]
+struct BadGuy {
+    name: String,
+    life: i32,
+    equipped_item: Item
+}
+
 fn main() {
     let sword = Item {
         name: "Big sword".to_string(),
@@ -25,6 +32,12 @@ fn main() {
         name: "Toto".to_string(),
         life: 100,
         inventory: vec![sword]
+    };
+
+    let bad_guy = BadGuy {
+        name: "Biff".to_string(),
+        life: 100,
+        equipped_item: sword,
     };
 
     println!("Hello, {our_hero:?}");
