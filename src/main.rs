5c3c507651bd79b98bfacbcc16735d15b125d133
Romain <romain.assie@ellona.io>
2022-01-14 17:34:03 +0100

    Implement our hero

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)]
 struct Perso {
     name: String,
     life: i32,
 }
 
 fn main() {
-    println!("Hello, world!");
+
+    let our_hero = Perso {
+        name: "Toto".to_string(),
+        life: 100
+    };
+
+    println!("Hello, {our_hero:?}");
 }
